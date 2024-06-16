c280a334a051e7277cbd20d525a4601e7f452235
cmmttd <forledwatch@gmail.com>
2024-06-16 15:16:06 +0200

    #3 Add some functionality

---
+++ frontend/lib/details_page.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Ex1App());
+}
+
+class Ex1App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Ex1 App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: DetailsScreen(),
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: IconButton(
+          icon: Icon(Icons.account_balance),
+          onPressed: () => print('abc'),
+        ),
+        // title: Text("App bar"),
+        backgroundColor: Colors.blue.shade300,
+        foregroundColor: Colors.blue.shade100,
+      ),
+      body: Ex1CheckWidget(),
+    );
+  }
+}
+
+class Ex1CheckWidget extends StatefulWidget {
+  double _progress = 0;
+  late double count;
+
+  @override
+  State<Ex1CheckWidget> createState() => _Ex1CheckWidgetState();
+}
+
+class _Ex1CheckWidgetState extends State<Ex1CheckWidget> {
+  void done() {
+    setState(() {
+      widget._progress += widget.count;
+    });
+  }
+
+  void undone() {
+    setState(() {
+      widget._progress -= widget.count;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var list = [
+      Text("Some progress: "),
+      // Expanded(
+      //     child: LinearProgressIndicator(
+      //       value: widget._progress,
+      //       minHeight: 1.0,
+      //     )),
+      Padding(
+          // padding: EdgeInsets.all(2),
+          padding: EdgeInsets.fromLTRB(4,2,4,0),
+          child: LinearProgressIndicator(
+            color: Colors.blue,
+            value: widget._progress,
+            minHeight: 6.0,
+          )),
+    ];
+
+    var pointsList = [
+      Ex1CheckPoint(label: 'first', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'sec', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'th', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'frth', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'ffth', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'first', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'sec', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'th', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'frth', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'ffth', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'first', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'sec', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'th', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'frth', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'first', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'sec', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'th', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'frth', onDone: () => done(), onUndone: () => undone()),
+      Ex1CheckPoint(label: 'last', onDone: () => done(), onUndone: () => undone()),
+    ];
+    widget.count = 1 / pointsList.length;
+
+    var customScrollView = CustomScrollView(
+      slivers: <Widget>[
+        SliverPersistentHeader(
+          pinned: true,
+          delegate: PersistentHeader(
+            widget: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: list,
+            ),
+          ),
+        ),
+        SliverList.builder(
+          // separatorBuilder: (context, index) => const Divider(),
+          itemCount: pointsList.length,
+          itemBuilder: (context, index) {
+            return Expanded(
+              child: Card(
+                color: Colors.blue.shade100,
+                child: pointsList[index],
+              ),
+            );
+          },
+        ),
+      ],
+    );
+
+    // list.addAll(pointsList);
+    // list.add(customScrollView);
+
+    // return Expanded(
+    //   child: list,
+    // );
+    return customScrollView;
+  }
+}
+
+class PersistentHeader extends SliverPersistentHeaderDelegate {
+  final Widget? widget;
+
+  PersistentHeader({this.widget});
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      // width: double.infinity,
+      // height: 106.0,
+      child: Card(
+        // margin: EdgeInsets.all(0),
+        // color: Colors.white,
+        // elevation: 5.0,
+        child: Center(child: widget),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 40.0;
+
+  @override
+  double get minExtent => 40.0;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
+
+class Ex1CheckPoint extends StatefulWidget {
+  final String label;
+  final void Function() onDone;
+  final void Function() onUndone;
+
+  const Ex1CheckPoint({super.key, required this.label, required this.onDone, required this.onUndone});
+
+  @override
+  State<Ex1CheckPoint> createState() => _Ex1CheckPointState();
+}
+
+class _Ex1CheckPointState extends State<Ex1CheckPoint> {
+  bool? _val = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+            height: 42,
+            child: Row(
+              children: [
+                Checkbox(
+                  // checkColor: Colors.blue,
+                  // focusColor: Colors.blue,
+                  // hoverColor: Colors.red,
+                  activeColor: Colors.blue,
+                  value: _val,
+                  onChanged: (value) {
+                    setState(() {
+                      _val = value;
+                    });
+                    if (value!) {
+                      widget.onDone.call();
+                    } else {
+                      widget.onUndone.call();
+                    }
+                  },
+                ),
+                Text(widget.label)
+              ],
+            ))
+      ],
+    );
+  }
+}
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,198 +1,97 @@
-import 'package:first_dart/search_screen.dart';
+import 'package:first_dart/details_page.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:parallax_animation/parallax_animation.dart';
-
-import 'main_gpt_2.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const CheckApp());
 }
 
-class MyApp extends StatelessWidget {
+class CheckApp extends StatelessWidget {
+  final title = "the name";
+
+  const CheckApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Search and Timeline App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  late final Future<String> json;
-
-  @override
-  void initState() {
-    super.initState();
-    json = obtainJson();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> list = [
-      // firstPage(),
-      HomeScreenGpt(),
-      const SecondPage(),
-    ];
-
-    final colors = [Colors.white, Colors.white, Colors.white];
-    final scrollController = PageController();
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
-        title: Text('App logo'),
-        centerTitle: true,
-      ),
-      body: ParallaxArea(
-        child: Scrollbar(
-          controller: scrollController,
-          child: PageView.builder(
-              itemCount: 2,
-              controller: scrollController,
-              scrollDirection: Axis.vertical,
-              itemBuilder: (context, index) {
-                return ParallaxWidget(
-                  overflowWidthFactor: 1,
-                  background: ColoredBox(
-                    color: colors[index],
-                  ),
-                  child: Center(
-                    child: list[index],
-                  ),
-                );
-              }),
-        ),
-      ),
-    );
-  }
-
-  Column firstPage() {
-    return Column(
-      children: [
-        Expanded(flex: 1, child: SizedBox(width: 600, child: SearchScreen())),
-        buildTree(),
-      ],
-    );
-  }
-
-  Padding infoCard() {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Card(
-          color: Colors.blue,
-          elevation: 5,
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Text(
-              "Welcome to the <> portal. Let's check your politician",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-                fontWeight: FontWeight.w900,
+        title: title,
+        debugShowCheckedModeBanner: false,
+        // theme: ThemeData.light(useMaterial3: true),
+        home: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: Scaffold(
+              appBar: AppBar(
+                title: Text(title),
+                backgroundColor: Colors.blue.shade300,
               ),
-            ),
-          )),
-    );
-  }
-
-  Expanded buildTree() {
-    return Expanded(
-        flex: 6,
-        child: Align(
-            alignment: Alignment.topCenter,
-            child: FutureBuilder<String>(
-              future: json,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data.toString(),
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w900,
-                      ));
-                } else if (snapshot.hasError) {
-                  return infoCard();
-                }
-                return const Expanded(
-                    child: Column(children: [
-                  Text("fake loading"),
-                  CircularProgressIndicator(),
-                ]));
-              },
-            )));
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Text("Credentials and etc",
-        style: TextStyle(
-          color: Colors.black38,
-          fontSize: 26,
-          fontWeight: FontWeight.w900,
+              body: HomePage()),
         ));
   }
 }
 
-Future<String> obtainJson() async {
-  var response = await http.get(Uri.parse('http://localhost:8081/mock/promises/1'));
-  // var response = await http.get(Uri.parse('https://google.com'));
-  print('Resp: ${response.statusCode}');
-  if (response.statusCode == 200) {
-    print('Resp: ${response.body}');
-    // return Promises.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    // return jsonDecode(response.body);
-    return response.body;
-  } else {
-    throw Exception('Failed to load album');
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        SearchBar(),
+        Padding(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: SizedBox(
+                width: 800,
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Card(
+                      child: Padding(
+                          padding: EdgeInsets.all(50),
+                          child: Text(
+                              "Description how it may be used. Description how it may be used. Description how it may be used. Description how it may be used.")),
+                    )))),
+        Expanded(
+            child: Padding(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 30),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text("Credentials"),
+                ))),
+      ],
+    );
   }
 }
 
-class Promises {
-  final int politician_id;
+class SearchBar extends StatelessWidget {
+  const SearchBar({super.key});
 
-  // final List<Promise> promises;
-
-  const Promises({
-    required this.politician_id,
-    // required this.promises,
-  });
-
-  factory Promises.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'politician_id': int politician_id,
-        // 'promises': List<Promise> promises,
-      } =>
-        Promises(
-          politician_id: politician_id,
-          // promises: promises,
-        ),
-      _ => throw const FormatException('Failed to load album.'),
-    };
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 1200,
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: TextField(
+            onChanged: (str) => print('>> changed $str'),
+            onEditingComplete: () => print('>> edit complete'),
+            onSubmitted: (str) {
+              print('>> submitted $str');
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailsScreen()),
+              );
+            },
+            onTap: () => print('>> tap'),
+            onTapOutside: (v) => print('>> tap outside'),
+            // enableInteractiveSelection: true,
+            // textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+                labelText: 'Search',
+                labelStyle: TextStyle(backgroundColor: Colors.transparent),
+                border: const OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+                prefixIcon: const Icon(Icons.search),
+                hintText: "Name or birtdate",
+                hintStyle: TextStyle(color: Colors.grey.shade400),
+                alignLabelWithHint: true),
+          ),
+        ));
   }
-}
-
-class Promise {
-  final int timestamp;
-  final String promise_name;
-  final String description;
-
-  Promise({required this.timestamp, required this.promise_name, required this.description});
 }
