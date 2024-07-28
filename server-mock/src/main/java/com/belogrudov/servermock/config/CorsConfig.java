3f9725714ffb2a347fa36311e294eb6956f7945f
cmmttd <forledwatch@gmail.com>
2024-07-28 15:08:42 +0200

    #19 Update server-mock url

--- server-mock/src/main/java/com/belogrudov/servermock/config/CorsConfig.java
+++ server-mock/src/main/java/com/belogrudov/servermock/config/CorsConfig.java
@@ -1,11 +1,13 @@
 package com.belogrudov.servermock.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
@@ -14,6 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+        log.info("Configuring CORS to allow from: {}", allowedOrigins);
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
